45aff6f93eba69e1838430c455020b3b324ca92a
shravanip419 <shravanip319@gmail.com>
2026-01-04 21:01:00 +0530

    first

---
+++ frontend/my-app/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import "../styles/header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h3>Hey Shravani 👋</h3>
+      <input placeholder="Search college..." />
+    </div>
+  );
+};
+
+export default Header;
---
+++ frontend/my-app/src/Components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import "../styles/sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <h2>GradMap</h2>
+      <ul>
+        <li>🏠</li>
+        <li>🎓</li>
+        <li>📊</li>
+        <li>⭐</li>
+        <li>⚙️</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
